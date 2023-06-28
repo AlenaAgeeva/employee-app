8d698d52137eb8ffd42be4572018c9c3f49d820c
Alena Ageeva <alenaageevapv@gmail.com>
2023-06-28 10:49:04 -0300

    addded some new changes with state

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,27 +1,54 @@
+import { render } from "@testing-library/react";
 import "./employees-list-item.css";
-function EmployeesListItem({ name, salary, increase }) {
-  let clName = `list-group-item d-flex justify-content-between ${
-    increase === true ? "increase" : ""
-  } `;
-  return (
-    <li className={clName}>
-      <span className="list-group-item-label">{name}</span>
-      <input
-        type="text"
-        className="list-group-item-input"
-        defaultValue={salary}
-      />
-      <div className="d-flex justify-content-center align-items-center">
-        <button type="button" className="btn-cookie btn-sm ">
-          <i className="fas fa-cookie"></i>
-        </button>
+import { Component } from "react";
+class EmployeesListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      increase: false,
+    };
+  }
+  onIncrease = () => {
+    this.setState(({ increase }) => ({
+      increase: !increase,
+    }));
+  };
+  addLike = () => {
+    console.log("k");
+  };
 
-        <button type="button" className="btn-trash btn-sm ">
-          <i className="fas fa-trash"></i>
-        </button>
-        <i className="fas fa-star"></i>
-      </div>
-    </li>
-  );
+  render() {
+    const { name, salary } = this.props;
+    const { increase } = this.state;
+    let clName = `list-group-item d-flex justify-content-between like ${
+      increase === true ? "increase" : ""
+    } `;
+    return (
+      <li className={clName}>
+        <span className="list-group-item-label" onClick={this.addLike}>
+          {name}
+        </span>
+        <input
+          type="text"
+          className="list-group-item-input"
+          defaultValue={salary}
+        />
+        <div className="d-flex justify-content-center align-items-center">
+          <button
+            type="button"
+            className="btn-cookie btn-sm "
+            onClick={this.onIncrease}
+          >
+            <i className="fas fa-cookie"></i>
+          </button>
+
+          <button type="button" className="btn-trash btn-sm ">
+            <i className="fas fa-trash"></i>
+          </button>
+          <i className="fas fa-star"></i>
+        </div>
+      </li>
+    );
+  }
 }
 export default EmployeesListItem;
